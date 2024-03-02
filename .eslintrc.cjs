63793978480b5475029cd3155eee0f49af5881bf
isakiDev <gaspar.c.developer@gmail.com>
2024-03-02 06:30:10 -0300

    feat: zustand

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,7 @@ module.exports = {
     ],
     "rules": {
         "react/react-in-jsx-scope": "off",
-        "@typescript-eslint/explicit-function-return-type": "off"
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-floating-promises": "off"
     }
 }
