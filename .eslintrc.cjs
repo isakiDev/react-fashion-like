10e6e4989cbc3120870a2982c75a2c3a3ad46c4c
isakiDev <gaspar.c.developer@gmail.com>
2024-03-09 04:57:47 -0300

    chore: update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,6 +32,9 @@ module.exports = {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-floating-promises": "off",
         "@typescript-eslint/strict-boolean-expressions": "off",
-        "@typescript-eslint/no-confusing-void-expression": "off"
+        "@typescript-eslint/no-confusing-void-expression": "off",
+
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/no-non-null-assertion": "off"
     }
 }
