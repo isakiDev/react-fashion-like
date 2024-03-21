4c64f988a16e7d4f3b7d2dfb424b47f514505557
isakiDev <gaspar.c.developer@gmail.com>
2024-03-21 04:03:32 -0300

    add PostCreatorBox in section

--- src/posts/pages/PostsPage.tsx
+++ src/posts/pages/PostsPage.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react'
-import { CardPost, SkeletonPost } from '../components'
+import { CardPost, PostCreatorBox, SkeletonPost } from '../components'
 import { usePosts } from '..'
+import { useAuth } from '../../auth'
 
 export const PostsPage = () => {
   const { posts, onGetPosts } = usePosts()
+  const { user } = useAuth()
 
   useEffect(() => {
     onGetPosts()
@@ -11,10 +13,13 @@ export const PostsPage = () => {
 
   return (
     <section className='p-4 w-full max-w-[500px] space-y-2'>
+
+      {user && <PostCreatorBox user={user} />}
+
       {
         (!posts)
           ? (< SkeletonPost />)
-          : posts.map((post) => (
+          : posts?.map((post) => (
             <CardPost
               key={post.id}
               post={post}
