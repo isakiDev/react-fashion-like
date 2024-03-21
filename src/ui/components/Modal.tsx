5732a91c5486b8753163c9b367a30fedb9aab123
isakiDev <gaspar.c.developer@gmail.com>
2024-03-21 04:00:04 -0300

    add modal component

---
+++ src/ui/components/Modal.tsx
@@ -0,0 +1,22 @@
+interface Props {
+  children: JSX.Element | JSX.Element[]
+  className: string
+  onToggleModal: () => void
+}
+
+export const Modal = ({ children, className, onToggleModal }: Props) => {
+  return (
+    <>
+      <div
+        className='absolute inset-0 bg-slate-200/80 flex justify-center'
+        onClick={onToggleModal}
+      />
+
+      <div className='fixed left-0 right-0 max-w-[500px] mx-auto z-10'>
+        <div className={`bg-gray-50 shadow-lg rounded-lg ${className} mx-4 lg:mx-0`}>
+          {children}
+        </div>
+      </div>
+    </>
+  )
+}
