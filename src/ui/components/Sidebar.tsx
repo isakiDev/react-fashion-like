4268804b00dcea6a0a29e8e2fc91bc0dd4e2ba8d
isakiDev <gaspar.c.developer@gmail.com>
2024-03-21 04:03:17 -0300

    ordinate lines

--- src/ui/components/Sidebar.tsx
+++ src/ui/components/Sidebar.tsx
@@ -8,17 +8,18 @@ export const Sidebar = () => {
 
   return (
     <aside className='flex flex-col items-center w-16 h-screen py-8 space-y-8 bg-white dark:bg-gray-900 dark:border-gray-700 sticky top-0'>
-      <img className="object-cover w-8 h-8 rounded-full" src={user?.image} alt="User image" />
+      <img alt="User image" className="object-cover w-10 h-10 rounded-full" src={user?.image} />
       {
         SIDEBAR_LINKS.map(({ href, icon }) => {
           const onLogout = href === '/auth/login' ? onLogoutUser : undefined
 
           return (
             <NavLink
+              className={({ isActive }) => `${isActive && 'dark:bg-gray-800 bg-gray-100'} p-1.5 text-gray-500 focus:outline-nones transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800 hover:bg-gray-100`}
               key={href}
-              to={href}
               onClick={onLogout}
-              className={({ isActive }) => `${isActive && 'dark:bg-gray-800 bg-gray-100'} p-1.5 text-gray-500 focus:outline-nones transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800 hover:bg-gray-100`}>
+              to={href}
+            >
               {icon()}
             </NavLink>
           )
