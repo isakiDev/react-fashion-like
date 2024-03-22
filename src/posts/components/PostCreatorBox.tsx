332c99d4b5175afad01fcdc1df5ffff26a2a6d73
isakiDev <gaspar.c.developer@gmail.com>
2024-03-22 01:41:24 -0300

    add isSibmitted state

--- src/posts/components/PostCreatorBox.tsx
+++ src/posts/components/PostCreatorBox.tsx
@@ -1,7 +1,7 @@
 import { useBoundStore } from '../../store/bound.store'
-import { ImageIcon, Modal } from '../../ui/components'
+import { CloseIcon, ImageIcon, Modal } from '../../ui/components'
 import { type User } from '../../types'
-import { type FormEvent, useRef } from 'react'
+import { type FormEvent, useRef, useState } from 'react'
 import { usePosts } from '..'
 import { toast } from 'sonner'
 
@@ -15,6 +15,8 @@ export const PostCreatorBox = ({ user }: Props) => {
   const toggleModal = useBoundStore(state => state.toggleState)
   const isModalOpen = useBoundStore(state => state.isOpen)
 
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
   const fileSelect = useRef<HTMLInputElement>(null)
 
   const text = 'What do you want to talk about?'
@@ -26,18 +28,22 @@ export const PostCreatorBox = ({ user }: Props) => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
+    setIsSubmitted(true)
+
     const fields = new window.FormData(e.target as HTMLFormElement)
     const description = fields.get('description')?.toString()
-    const image = fields.get('image')?.toString()
+    // const file = fields.get('file') as File
 
     if (!description || description?.trim().length <= 0) return toast.error('Invalid description')
 
-    toast.promise(onCreatePost({ description, image }), {
+    toast.promise(onCreatePost(fields), {
+      loading: 'Creating post...',
       success: () => {
         toggleModal()
         return 'Post created'
       },
-      error: (error) => error.message
+      error: (error) => error.message,
+      finally: () => { setIsSubmitted(false) }
     })
   }
 
@@ -48,7 +54,7 @@ export const PostCreatorBox = ({ user }: Props) => {
         className="object-cover h-9 w-9 rounded-full"
         src={user?.image}
       />
-      <button className='flex-1 text-gray-400  bg-gray-200 border text-gray border-gray-400 rounded-full text-start pl-4' onClick={toggleModal}>Create Post</button>
+      <button className='flex-1 text-gray-400  bg-gray-200 border text-gray border-gray-400 rounded-full text-start pl-4' onClick={toggleModal}>Create post</button>
 
       {isModalOpen &&
         <Modal
@@ -61,7 +67,16 @@ export const PostCreatorBox = ({ user }: Props) => {
               className="object-cover h-14 w-1h-14 rounded-full"
               src={user?.image}
             />
+
             <h1 className='text-xl font-semibold text-gray-700'>{user.name}</h1>
+
+            <button
+              className='flex flex-1 h-0 justify-end text-gray-500 hover:text-gray-700 hover:cursor-pointer'
+              onClick={toggleModal}
+            >
+              <CloseIcon />
+            </button>
+
           </header>
 
           <form onSubmit={handleSubmit}>
@@ -82,15 +97,16 @@ export const PostCreatorBox = ({ user }: Props) => {
             <hr />
 
             <div className='flex justify-end pt-4'>
-              <button className='px-5 py-1 rounded-full flex- bg-blue-600 font-semibold text-gray-100'>Post</button>
+              <button
+                className='disabled:bg-gray-300 px-5 py-1 rounded-full bg-blue-600 font-semibold text-gray-100'
+                disabled={isSubmitted}
+              >Post</button>
             </div>
 
-            <input className='hidden' name='image' ref={fileSelect} type='file'/>
+            <input className='hidden' name='file' ref={fileSelect} type='file'/>
           </form>
-
         </Modal>
       }
-
     </section>
   )
 }
