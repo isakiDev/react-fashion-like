d30d493ca09b410d0fdd9165351936a007fa3a6f
isakiDev <gaspar.c.developer@gmail.com>
2024-03-24 03:00:08 -0300

    add messages to confirm page

--- src/auth/pages/ConfirmUserPage.tsx
+++ src/auth/pages/ConfirmUserPage.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
 
 import { useAuth } from '..'
-import { toast } from 'sonner'
-import { Link } from 'react-router-dom'
 
 export const ConfirmUserPage = () => {
   const { onVerifyUserEmail } = useAuth()
@@ -20,14 +20,28 @@ export const ConfirmUserPage = () => {
       error: (error) => error.message
     })
   }
+
   useEffect(() => {
     verifyUser()
   }, [])
 
+  const messages = [
+    { title: 'Thanks for signing up for Fashion Like!', msg: 'We are happy you are here. You can now log in with your account.' },
+    { title: 'Ups ☹️¡¡ Your account could not be verified', msg: 'Please contact the administrator' }
+  ] as const
+
+  const msgUserVerified = userVerified
+    ? messages[0]
+    : messages[1]
+
   return (
-    <section >
-      <h1>User</h1>
-      { userVerified && <Link to='/auth/login'>Login</Link>}
-    </section>
+    <div className="flex items-center justify-center min-h-screen p-5 bg-blue-100">
+      <div className="flex flex-col gap-4 p-8 text-center text-gray-800 bg-white shadow-xl lg:max-w-3xl rounded-3xl lg:p-12">
+        <h3 className="text-2xl">{msgUserVerified.title}</h3>
+        <p>{msgUserVerified.msg}</p>
+
+        {userVerified && <Link className='px-2 py-2 text-blue-200 bg-blue-600 rounded' to='/auth/login'>Login</Link>}
+      </div>
+    </div>
   )
 }
