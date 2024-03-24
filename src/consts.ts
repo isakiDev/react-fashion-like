6c853104bdddd7078497a2d208d1910ffaf91ec0
isakiDev <gaspar.c.developer@gmail.com>
2024-03-24 02:57:13 -0300

    add handleSubmitPassword to change password

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
+import { string } from 'yup'
 import { HomeIcon, LogoutIcon, SettingsIcon } from './ui/components'
 // import { type LoginRequest } from './types'
 
@@ -19,8 +20,13 @@ export const REGISTER_INITIAL_VALUES = {
   confirmPassword: ''
 }
 
-export const SETTINGS_INITIAL_VALUES = {
+export const SETTINGS_PERSONAL_INFORMATION = {
   name: ''
+
+}
+export const SETTINGS_CHANGE_PASSWORD = {
+  currentPassword: '',
+  newPassword: ''
 }
 
 export const SIDEBAR_LINKS = [
--- src/posts/pages/SettingsPage.tsx
+++ src/posts/pages/SettingsPage.tsx
@@ -1,12 +1,18 @@
 import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
 import { toast } from 'sonner'
 
 import { useAuth } from '../../auth'
-import { SETTINGS_INITIAL_VALUES } from '../../consts'
+import { SETTINGS_PERSONAL_INFORMATION, SETTINGS_CHANGE_PASSWORD } from '../../consts'
 import { ErrorMessageFormik, InputFormik } from '../../ui/components'
 
 interface UpdateInputs {
   name: string
+}
+
+interface UpdateInputsPassword {
+  currentPassword: string
+  newPassword: string
 }
 
 export const SettingsPage = () => {
@@ -20,22 +26,33 @@ export const SettingsPage = () => {
     })
   }
 
+  const handleSubmitPassword = ({ currentPassword, newPassword }: UpdateInputsPassword) => {
+    toast.promise(onUpdateUser({ currentPassword, newPassword }), {
+      success: 'Password updated',
+      loading: 'Updating password...',
+      error: (error) => error.message
+    })
+  }
+
   return (
     <section className='flex flex-col gap-8 p-4 bg-white dark:text-white dark:bg-gray-900 w-full max-w-[500px]'>
       <div className='flex gap-8'>
         <img alt="User Image" className='object-cover w-20 h-20 rounded-md' src={user?.image} />
-        <div className='space-y-2'>
+        {/* <div className='space-y-2'>
           <button className='px-2 py-1 font-semibold bg-gray-700 hover:bg-gray-600 rounded-md'>Change Image</button>
           <p className='text-sm text-gray-400'>JPG, GIF or PNG. 1MB max.</p>
-        </div>
+        </div> */}
       </div>
 
       <div>
         <h1 className='pb-3 font-semibold'>Personal Information</h1>
 
         <Formik
-          initialValues={SETTINGS_INITIAL_VALUES}
+          initialValues={SETTINGS_PERSONAL_INFORMATION}
           onSubmit={handleSubmit}
+          validationSchema={Yup.object({
+            name: Yup.string().trim().required()
+          })}
         >
           {
             () => (
@@ -43,25 +60,40 @@ export const SettingsPage = () => {
                 <div className='flex flex-col gap-4'>
                   <InputFormik className='px-2 py-1 rounded-md bg-[#1d2432] border border-[#262e3a]' name='name' placeholder={user?.name} type='text' />
                   <ErrorMessageFormik component='span' name='name' />
+
+                  <input className=' disabled:bg-transparent px-2 py-1 rounded-md bg-[#1d2432] border border-[#262e3a]' disabled value={user?.email}/>
                 </div>
-                <button className='px-4 py-1 my-8 rounded-md bg-slate-600' type='submit'>Save</button>
+                <button className='px-4 py-1 my-8 rounded-md bg-slate-600 hover:bg-slate-500' type='submit'>Save</button>
               </Form>
             )
           }
         </Formik>
       </div>
 
-      {/* <div>
-        <h1 className='pb-3 font-semibold'>Change password</h1>
-        <form>
-          <div className='flex flex-col gap-4'>
-            <input className='px-2 py-1 rounded-md bg-[#1d2432] border border-[#262e3a]' placeholder="Current password" type="password" />
-            <input className='px-2 py-1 rounded-md bg-[#1d2432] border border-[#262e3a]' placeholder="New password" type="password" />
-            <input className='px-2 py-1 rounded-md bg-[#1d2432] border border-[#262e3a]' placeholder="Confirm password" type="password" />
-          </div>
-          <button className='px-4 py-1 my-8 rounded-md bg-slate-600'>Save</button>
-        </form>
-      </div> */}
+      <Formik
+        initialValues={SETTINGS_CHANGE_PASSWORD}
+        onSubmit={handleSubmitPassword}
+        validationSchema={Yup.object({
+          currentPassword: Yup.string().trim().required('current password is a required field'),
+          newPassword: Yup.string().trim().required('new password is a required field')
+        })}
+      >
+        {
+          () => (
+            <Form>
+              <h1 className='pb-3 font-semibold'>Change password</h1>
+              <div className='flex flex-col gap-4'>
+                <InputFormik className='px-2 py-1 rounded-md bg-[#1d2432] border border-[#262e3a]' name='currentPassword' placeholder='Enter current password' type='password' />
+                <ErrorMessageFormik component='span' name='currentPassword' />
+                <InputFormik className='px-2 py-1 rounded-md bg-[#1d2432] border border-[#262e3a]' name='newPassword' placeholder='Enter new password' type='password' />
+                <ErrorMessageFormik component='span' name='newPassword' />
+              </div>
+              <button className='px-4 py-1 my-8 rounded-md bg-slate-600 hover:bg-slate-500'>Save</button>
+            </Form>
+          )
+        }
+      </Formik>
+
     </section>
   )
 }
