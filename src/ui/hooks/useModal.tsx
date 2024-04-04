eb7ad73c3dfcce70f11d7ff5d953cbb71b525b67
isakiDev <gaspar.c.developer@gmail.com>
2024-04-04 03:32:22 -0300

    feat: create hook Modal

---
+++ src/ui/hooks/useModal.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export const useModal = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
+  const toggleModal = () => {
+    setIsOpenModal(!isOpenModal)
+  }
+
+  return {
+    isOpenModal,
+
+    toggleModal
+  }
+}
