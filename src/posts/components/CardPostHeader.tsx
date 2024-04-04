f40330d7bff4bdfdcea363ccdb4a37c6a772f450
isakiDev <gaspar.c.developer@gmail.com>
2024-04-04 03:28:24 -0300

    update styles

--- src/posts/components/CardPostHeader.tsx
+++ src/posts/components/CardPostHeader.tsx
@@ -1,33 +1,33 @@
 import { CardPostActions } from '.'
+import { type PostsResponse } from '../../types'
 import { UserImage } from '../../ui/components/Image/UserImage'
 
 interface Props {
-  url: string
-  name: string
-  description: string
-  date: string
-  showActions: boolean
+  post: PostsResponse
+  showActions?: boolean
 }
 
-export const CardPostHeader = ({ showActions, url, name, description, date }: Props) => {
-  const newDate = new Date(date).toDateString()
+export const CardPostHeader = ({ post, showActions = false }: Props) => {
+  const { user, description, createdAt } = post
+
+  const newDate = new Date(createdAt).toDateString()
 
   return (
     <header className="flex flex-col px-4 py-3 gap-4">
       <div className='flex justify-between'>
-        <div className='flex'>
-          <UserImage className='h-8 w-8' src={url} />
+        <div className='flex items-center'>
+          <UserImage className='h-8 w-8' src={user?.image} />
           <div className="flex flex-col ml-3">
-            <h2 className="text-sm font-semibold">{name}</h2>
+            <h2 className="text-sm font-semibold">{user?.name}</h2>
             <span className='text-xs text-gray-500'>{newDate}</span>
           </div>
         </div>
 
-        {showActions && <CardPostActions />}
+        {showActions && <CardPostActions post={post} />}
 
       </div>
 
-      <p>{description}</p>
+      <p className=' whitespace-pre-line break-all'>{description}</p>
     </header>
   )
 }
