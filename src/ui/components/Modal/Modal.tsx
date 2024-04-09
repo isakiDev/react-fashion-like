6070eea3b674fcc78c4ec6815e0ac86918851481
isakiDev <gaspar.c.developer@gmail.com>
2024-04-08 22:46:04 -0400

    update: add z-index in modal

--- src/ui/components/Modal/Modal.tsx
+++ src/ui/components/Modal/Modal.tsx
@@ -8,11 +8,11 @@ export const Modal = ({ children, className, onToggleModal }: Props) => {
   return (
     <>
       <div
-        className='fixed inset-0 bg-slate-200/80 flex justify-center'
+        className='fixed inset-0 bg-slate-200/80 flex justify-center z-10'
         onClick={onToggleModal}
       />
 
-      <div className='fixed top-10 left-0 right-0 max-w-[500px] mx-auto z-10 overflow-y-auto h-screen px-4'>
+      <div className='fixed top-10 left-0 right-0 max-w-[500px] mx-auto z-20 overflow-y-auto px-4'>
         <div className={`bg-gray-50 shadow-lg rounded-lg ${className} `}>
           {children}
         </div>
